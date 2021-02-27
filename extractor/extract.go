d6ecb683bbd4bf3e86a143970e9a8f16416e00f5
Evan Cleary <evan.cleary@hotmail.com>
2021-02-26 20:50:59 -0500

    Remove sdk1

--- extractor/extract.go
+++ extractor/extract.go
@@ -1,7 +1,7 @@
 package extractor
 
 import (
-	sdk "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	// sdk "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	sdk2 "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	tf "github.com/hashicorp/terraform/helper/schema"
 )
@@ -12,11 +12,11 @@ func ExtractProvider(provider interface{}, pi *ProviderInfo, outputPath string) 
 		extractor := new(Extractor)
 		extractor.Generate(tfp, pi, outputPath)
 	}
-	sdkp, ok := provider.(*sdk.Provider)
-	if ok {
-		extractor := new(SdkExtractor)
-		extractor.Generate(sdkp, pi, outputPath)
-	}
+	// sdkp, ok := provider.(*sdk.Provider)
+	// if ok {
+	// 	extractor := new(SdkExtractor)
+	// 	extractor.Generate(sdkp, pi, outputPath)
+	// }
 	sdk2p, ok := provider.(*sdk2.Provider)
 	if ok {
 		extractor := new(Sdk2Extractor)
